b49734a9e91588da38af6a0bb407a3bfc32dd1aa
AllaGrey <alla.basanko2012@gmail.com>
2023-01-30 23:09:05 +0200

    add 2, 3 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// iframe.addEventListener('player', throttle(7000));
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(e) {
+  const currentTime = e.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}
+
+const savedTimeFromLocalStorage = Number(
+  localStorage.getItem('videoplayer-current-time')
+);
+console.log(savedTimeFromLocalStorage);
+
+player.setCurrentTime(savedTimeFromLocalStorage).then(function (seconds) {
+  // seconds = the actual time that the player seeked to
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const feedbackFormState = {};
+const STORAGE_KEY = 'feedback-form-state';
+
+initFeedbackFormState();
+
+form.addEventListener('submit', throttle(onFormSubmit, 500));
+form.addEventListener('input', throttle(onFormChange, 500));
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  if (e.target.email.value === '' || e.target.message.value === '') {
+    return alert('Please, fill all fields!');
+  } else {
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+  }
+}
+
+function onFormChange(e) {
+  feedbackFormState[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackFormState));
+}
+
+function initFeedbackFormState() {
+  let savedFeedbackFormState = localStorage.getItem(STORAGE_KEY);
+
+  if (savedFeedbackFormState) {
+    savedFeedbackFormState = JSON.parse(savedFeedbackFormState);
+    form.elements.email.textContent = savedFeedbackFormState.email;
+    form.elements.message.textContent = savedFeedbackFormState.message;
+  }
+}
